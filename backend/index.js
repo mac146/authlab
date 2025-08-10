957b8a7df811a2e8e36ed3cb8b045d0c22dbffde
mayank <mayankkumars584@gmail.com>
2025-08-10 10:07:24 +0530

    changed the port

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ console.log(" mongoose_url:", process.env.mongoose_url);
 const bcrypt = require('bcrypt')
 const express = require('express')
 const jwt = require('jsonwebtoken')
+const cors = require('cors');
 
 const {userRouter}=require("./routes/user")
 const mongoose = require('mongoose');
@@ -12,12 +13,16 @@ const app = express()
 
 
 app.use(express.json());
-
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use("/user",userRouter)
 
 async function main(){
    await mongoose.connect(process.env.mongoose_url)
-    app.listen(3000);
-    console.log("listening on port 3000")
+    app.listen(5000);
+    console.log("listening on port 5000")
 }
 main();
