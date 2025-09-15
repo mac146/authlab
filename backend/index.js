63b86f590ab37f65c14e0a11c9ae9b37b1c47164
mayank <mayankkumars584@gmail.com>
2025-09-15 16:43:47 +0530

    fixed err

--- backend/index.js
+++ backend/index.js
@@ -5,20 +5,13 @@ const bcrypt = require('bcrypt')
 const express = require('express')
 const jwt = require('jsonwebtoken')
 const cors = require('cors');
-
+const app = express();
 const {userRouter}=require("./routes/user")
 const mongoose = require('mongoose');
 
 app.use(express.json());
 
-app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://auth-frontend-three-peach.vercel.app"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));
+app.use(cors());
 app.use("/user",userRouter)
 
 async function main(){
